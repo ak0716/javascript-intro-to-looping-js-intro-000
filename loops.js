b9b812a31dae319bbe4672d993600dabfbf7d0c9
Ariel Reuven Kogan <ar.kogan@gmail.com>
2020-09-15 20:10:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ var array = []
 
 function forLoop(array) {
   for(let i = 0; i < 25 < i++) {
-    console.log(`\"I am ${i} strange loops"`)
+    console.log(`"I am ${i} strange loops."`)
   }
   return [array, 25]
 }
