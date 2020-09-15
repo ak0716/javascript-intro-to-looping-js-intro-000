2e3b6b4c0a651014f44b333c8325206cfddf7b67
Ariel Reuven Kogan <ar.kogan@gmail.com>
2020-09-15 21:26:29 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -21,6 +21,6 @@ return i;
 }
 
 function doWhileLoop(num) {
-  do {console.log("I run once regardless.")}
-  while (incrementVariable() < num)
+  do {console.log("I run once regardless.")};
+  while (incrementVariable() < num);
 }
